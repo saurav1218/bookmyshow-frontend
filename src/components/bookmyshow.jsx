6bbd4514500077f904877bd8c721ab49b714cdf6
saurav1218 <sauravmishra1218@gmail.com>
2023-08-28 01:11:31 +0530

    update

--- src/components/bookmyshow.jsx
+++ src/components/bookmyshow.jsx
@@ -70,7 +70,6 @@ const Bookmyshow = () => {
                 <h3>Agent</h3>
                 <p>Action/Thriller</p>
             </div>
-            <img src="" alt="" />
             <div class="section">
                 <img src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny43LzEwICAxMy4zSyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00349309-kzclvwxuhc-portrait.jpg" alt="" />
                 <h3>Evil Dead Rise</h3>

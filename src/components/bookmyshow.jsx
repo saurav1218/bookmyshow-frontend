e1bfd3f388fb8e6ca7439d0d59c96a4b3e014f0c
saurav1218 <sauravmishra1218@gmail.com>
2024-02-22 11:45:01 +0530

    update

--- src/components/bookmyshow.jsx
+++ src/components/bookmyshow.jsx
@@ -51,9 +51,9 @@ const Bookmyshow = () => {
 
         <div class="content2">
             <div class="section">
-                <img src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@like_202006280402.png,ox-24,oy-617,ow-29:ote-MjQ2LjJLIGxpa2Vz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00348725-grqrbbqnrs-portrait.jpg" alt="" />
-                <h3>Ponniyin Selvan - Part 2</h3>
-                <p>Action/Adventure/Drama /Historical</p>
+                <img src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC4yLzEwICA4OC42SyBWb3Rlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00383266-wrllgjszgu-portrait.jpg" alt="" />
+                <h3>Teri Baaton Mein Aisa Uljha jiya</h3>
+                <p>Comedy/Family/Romantic</p>
             </div>
             <div class="section">
                 <img src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ni44LzEwICA1OS41SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00339118-gvcavdpszx-portrait.jpg" alt=""/>
